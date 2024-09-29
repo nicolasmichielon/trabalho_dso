ee0fd4a0e1d3d7559ea1b436ba9089be2eb8da05
Nícolas Michielon <61835002+nicolasmichielon@users.noreply.github.com>
2024-09-29 20:11:10 +0000

    Add Pessoa class with getters and setters

---
+++ pessoa.py
@@ -0,0 +1,45 @@
+class Pessoa:
+    def __init__(self, nome: str, telefone: str, cpf: str, idade: int):
+        self.__nome = nome
+        self.__telefone = telefone
+        self.__cpf = cpf
+        self.__idade = idade
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self, telefone: str = None):
+        if telefone is not None:
+            self.__telefone = telefone
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str = None):
+        if nome is not None:
+            self.__nome = nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf: str = None):
+        if cpf is not None:
+            self.__cpf = cpf
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade: int = None):
+        if idade is not None:
+            self.__idade = idade
+
+    def fazer_aniversario(self):
+        self.__idade += 1
